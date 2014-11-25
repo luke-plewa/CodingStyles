9d8e7b6c1a58dba8bcca03170d45153e9e10152a
luke-plewa <luke.plewa@gmail.com>
2014-11-25 15:13:34 -0800

    add dict word counts

--- parser.py
+++ parser.py
@@ -7,9 +7,12 @@ from nltk.corpus import words
 rootdir = '/Users/luke/Documents/582/CodingStyles/samples/'
 file_data = dict()
 repositories = ['git', 'php-src', 'redis', 'scikit-learn']
+
 decl_words = defaultdict(lambda: 0)
 decl_lines = defaultdict(lambda: list())
+decl_repos = defaultdict(lambda: list())
 decl_vars = dict()
+dict_count = defaultdict(lambda: list())
 
 keywords = [
   "auto", "break", "case", "char", "const", "continue", "default", "do",
@@ -36,6 +39,13 @@ class MyVariable:
   def __init__(self, name):
     self.name = name
 
+  def repos(self):
+    my_repos = list()
+    for repo in repositories:
+      if self.name in decl_repos[repo]:
+        my_repos.append(repo)
+    return my_repos
+
   def snake(self):
     for word in self.name:
       if "_" in word:
@@ -59,12 +69,13 @@ class MyVariable:
     return dict_words
 
   def __str__(self):
-    return str([
-      self.name,
-      '\t', "Camel: ", self.camel(),
-      '\t', "Snake: ", self.snake(),
-      '\t', "Dict Words: ", self.dict_words(),
-    ])
+    return str(
+      self.name +
+      "\tCamel: " + str(self.camel()) +
+      "\tSnake: " + str(self.snake()) +
+      "\tDict Words: " + str(self.dict_words()) +
+      "\n\tRepositories: " + str(self.repos())
+    )
 
 class MyDocument:
   '''represents a code document'''
@@ -81,6 +92,7 @@ def main():
   parse()
   pattern_detect()
   process_dict()
+  report()
 
 # add files to public dict
 # remove empty directories and non-C files
@@ -118,10 +130,10 @@ def pattern_detect():
             last_index = 0
             for word in my_line:
               if word == "=":
-                add_word_to_dictionaries(line, my_line[last_index - 1])
+                add_word_to_dictionaries(line, my_line[last_index - 1], repo)
               last_index += 1
 
-def add_word_to_dictionaries(my_line, decl_word):
+def add_word_to_dictionaries(my_line, decl_word, repo):
   # add code for extracting the words before/after the keyword
   if (re.search(r".*\[([a-zA-Z0-9]+)\].*", decl_word) or
       re.search(r".*->.*", decl_word) or
@@ -129,27 +141,33 @@ def add_word_to_dictionaries(my_line, decl_word):
     print("array assign: " + decl_word)
     return
 
-  replacement_chars = ["*", "(", ")", "{", "}", "[", "]"]
+  replacement_chars = ["*", "(", ")", "{", "}", "[", "]", "++", "--"]
 
   for char in replacement_chars:
     decl_word = decl_word.replace(char, "")
   decl_words[decl_word] += 1
-  decl_lines[decl_word].append(my_line)
+  decl_lines[decl_word].append((my_line, repo))
   print(decl_word)
 
 def process_dict():
+  for word in decl_words:
+    decl_vars[word] = MyVariable(word)
+    for repo in repositories:
+      if word not in decl_repos[repo]:
+        decl_repos[repo].append(word)
+        if len(decl_vars[word].dict_words()) > 0:
+          dict_count[repo] = (dict_count[repo][0], dict_count[repo][1] + 1)
+
   sorted_words = sorted(
     list(decl_words.keys()),
     key=lambda k: decl_words[k],
     reverse=True
   )
 
-  for word in decl_words:
-    decl_vars[word] = MyVariable(word)
+  # for word in sorted_words[:300]:
+  #   print(decl_vars[word])
 
-  for word in sorted_words[:100]:
-    print(decl_vars[word])
-
-  import pdb; pdb.set_trace()
+def report():
+  print(dict_count)
 
 main()
